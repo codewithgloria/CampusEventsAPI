c52074eab3553896f0b93ed4c9f50b1d339e181c
Gloria <abinezagloria@gmail.com>
2025-09-05 01:25:16 +0300

    created customuser model

--- event_management_api/users/models.py
+++ event_management_api/users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
-# Create your models here.
+# user roles
+ROLE_CHOICES = [
+    ('student', 'Student'),
+    ('faculty', 'Faculty'),
+    ('admin', 'Admin'),
+]
+
+class CustomUser(AbstractUser):
+    
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default='student'
+    )
+    department = models.CharField(max_length=100, blank=True)
+
+    objects = CustomUserManager()  # assigned custom manager
+
+    # to fix reverse accessor clashes
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_permissions_set',
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
